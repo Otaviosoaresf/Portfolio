78cf67bdedaefacf98e5f95ef12ac212a3d1d51d
unknown <otvio2018@outlook.com>
2024-02-03 11:39:29 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,16 @@
+//const elementoClicavel = document.querySelectorAll(".container__titulo")
+const displayFront = document.querySelector(".front")
+const displayCsharp = document.querySelector(".Csharp")
+const tituloClicavelfront = document.getElementById("titulofront")
+const tituloClicavelCsharp = document.getElementById("tituloCsharp")
+
+tituloClicavelfront.addEventListener('click', () => {
+    displayFront.style.display = 'block'
+    displayCsharp.style.display = 'none'
+})
+
+tituloClicavelCsharp.addEventListener('click', () => {
+    displayCsharp.style.display = 'block'
+    displayFront.style.display = 'none'
+})
+
